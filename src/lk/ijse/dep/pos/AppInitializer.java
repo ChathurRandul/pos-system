a8869c45d55a9a0cad57c726d5fa5407a68923ee
Chathur <chathur.randul@gmail.com>
2020-02-25 21:50:06 +0530

    Update AppInitializer

--- src/lk/ijse/dep/pos/AppInitializer.java
+++ src/lk/ijse/dep/pos/AppInitializer.java
@@ -6,25 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import lk.ijse.dep.pos.db.HibernateUtil;
 
 import java.net.URL;
-import java.sql.SQLException;
-import java.util.logging.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class AppInitializer extends Application {
 
     public static void main(String[] args) {
         launch(args);
-        try {
-            System.out.println("Shutting down the connection");
-            DBConnection.getInstance().getConnection().close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Shutting down the connection");
+        HibernateUtil.getSessionFactory().close();
     }
 
     @Override
-    public void start(Stage primaryStage)  {
+    public void start(Stage primaryStage) {
         try {
 
             // Let's setup the root logger
@@ -34,20 +33,19 @@ public class AppInitializer extends Application {
             fileHandler.setLevel(Level.INFO);
             rootLogger.addHandler(fileHandler);
 
-            DBConnection.getInstance().getConnection();
             URL resource = this.getClass().getResource("/lk/ijse/dep/pos/view/MainForm.fxml");
             Parent root = FXMLLoader.load(resource);
             Scene mainScene = new Scene(root);
             primaryStage.setScene(mainScene);
-            primaryStage.setTitle("POS System");
+            primaryStage.setTitle("JDBC POS - 2019");
             primaryStage.centerOnScreen();
-            primaryStage.setResizable(true);
+            primaryStage.setResizable(false);
             primaryStage.centerOnScreen();
             primaryStage.show();
 
-        }catch (Exception e){
-            new Alert(Alert.AlertType.ERROR,"Something went wrong, please contact Developer Team").show();
-            Logger.getLogger("lk.ijse.dep.pos").log(Level.SEVERE, null,e);
+        } catch (Exception e) {
+            new Alert(Alert.AlertType.ERROR, "Something went wrong, please contact DEPPO").show();
+            Logger.getLogger("lk.ijse.dep.pos").log(Level.SEVERE, null, e);
         }
     }
 }
