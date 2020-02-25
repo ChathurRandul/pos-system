7916663fbc764eb7adbba829d35bbef84c55b4a0
Chathur <chathur.randul@gmail.com>
2020-02-25 22:04:19 +0530

    Update Entity.Customer

--- src/lk/ijse/dep/pos/entity/Customer.java
+++ src/lk/ijse/dep/pos/entity/Customer.java
@@ -1,9 +1,6 @@
 package lk.ijse.dep.pos.entity;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
