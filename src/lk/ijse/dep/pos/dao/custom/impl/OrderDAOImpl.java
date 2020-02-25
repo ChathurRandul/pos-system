c2fe07b0e9eb3516c1892e74edc039a23d7f3621
Chathur <chathur.randul@gmail.com>
2020-02-25 22:19:55 +0530

    Revamp DAO Layer 02

--- src/lk/ijse/dep/pos/dao/custom/impl/OrderDAOImpl.java
+++ src/lk/ijse/dep/pos/dao/custom/impl/OrderDAOImpl.java
@@ -1,19 +1,13 @@
 package lk.ijse.dep.pos.dao.custom.impl;
 
+import lk.ijse.dep.pos.dao.CrudDAOImpl;
 import lk.ijse.dep.pos.dao.custom.OrderDAO;
 import lk.ijse.dep.pos.entity.Order;
 import org.hibernate.Session;
 
 import java.util.List;
 
-public class OrderDAOImpl implements OrderDAO {
-
-    private Session session;
-
-    @Override
-    public void setSession(Session session) {
-        this.session = session;
-    }
+public class OrderDAOImpl extends CrudDAOImpl<Order, Integer> implements OrderDAO {
 
     @Override
     public int getLastOrderId() throws Exception {
@@ -26,30 +20,5 @@ public class OrderDAOImpl implements OrderDAO {
     public boolean existsByCustomerId(String customerId) throws Exception {
         return session.createNativeQuery("SELECT * FROM `Order` WHERE id=?1")
                 .setParameter(1, customerId).uniqueResult() != null;
-    }
-
-    @Override
-    public List<Order> findAll() throws Exception {
-        return session.createQuery("FROM Order", Order.class).list();
-    }
-
-    @Override
-    public Order find(Integer orderId) throws Exception {
-        return session.find(Order.class, orderId);
-    }
-
-    @Override
-    public void save(Order order) throws Exception {
-        session.save(order);
-    }
-
-    @Override
-    public void update(Order order) throws Exception {
-        session.merge(order);
-    }
-
-    @Override
-    public void delete(Integer orderId) throws Exception {
-        session.delete(session.load(Order.class, orderId));
     }
 }
